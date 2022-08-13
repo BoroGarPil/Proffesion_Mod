92080d35fc83b01e947dce285f0b1267cf3d3ea4
garbo <garbosborys@gmail.com>
2022-08-13 20:08:26 +0200

    Added files

---
+++ src/main/java/net/boro/professionmod/ProfessionMod.java
@@ -0,0 +1,15 @@
+package net.boro.professionmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ProfessionMod implements ModInitializer {
+	public static final String MOD_ID = "professionmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/boro/professionmod/ProffesionModClient.java
@@ -0,0 +1,11 @@
+package net.boro.professionmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ProffesionModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
